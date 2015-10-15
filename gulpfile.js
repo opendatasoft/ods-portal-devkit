a0501f8cf88d291663b6ffe9f355c93f0586b313
Benoit Richter <benoit.richter@opendatasoft.com>
2015-10-15 19:22:50 +0200

    Added lessc and autoprefixer to the pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp');
+
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+
 var config = require('./config');
 
 gulp.task('default', ['help']);
@@ -6,7 +10,7 @@ gulp.task('default', ['help']);
 gulp.task('help', function() {
     //console.log(config.PORTAL_HOST)
     // TODO: Document the available tasks
-    console.log('You can do this.. or that...')
+    console.log('You can do this.. or that...');
 });
 
 gulp.task('watch', function() {
@@ -16,4 +20,9 @@ gulp.task('watch', function() {
 gulp.task('push', function() {
     // Compile the LESS files into a single CSS file, push it to the remote
     // API.
+    gulp.src('src/less/main.less')
+        .pipe(less())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('build'));
+
 })
