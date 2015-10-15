8f7b618bdb5b9967f073eb7dbb78154d7c43df80
Benoit Richter <benoit.richter@opendatasoft.com>
2015-10-15 18:27:13 +0200

    Placeholder tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
 var gulp = require('gulp');
 var config = require('./config');
 
-gulp.task('default', function() {
-    // place code for your default task here
-    console.log(config.PORTAL_HOST)
+gulp.task('default', ['help']);
+
+gulp.task('help', function() {
+    //console.log(config.PORTAL_HOST)
+    // TODO: Document the available tasks
+    console.log('You can do this.. or that...')
 });
+
+gulp.task('watch', function() {
+    // TODO: Watch, and if a LESS file changes, push it.
+});
+
+gulp.task('push', function() {
+    // Compile the LESS files into a single CSS file, push it to the remote
+    // API.
+})
