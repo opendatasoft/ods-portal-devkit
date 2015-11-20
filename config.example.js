a76fc77039477cc3fda4be742ad9a8ef4236e388
Benoit Richter <benoit.richter@opendatasoft.com>
2015-11-20 15:34:21 +0100

    Change default config to use production

--- config.example.js
+++ config.example.js
@@ -8,6 +8,6 @@ module.exports = {
 
     // You usually shouldn't have to edit these
     ODS_PORTAL_PROTOCOL: 'http:',
-    ODS_PORTAL_SUFFIX: '.ods.com',
-    ODS_PORTAL_PORT: 8000
+    ODS_PORTAL_SUFFIX: '.opendatasoft.com',
+    ODS_PORTAL_PORT: 80
 };
