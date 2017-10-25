b2d61596d44ac117ef1f7541378789886e64d913
Jeremy Morel <jeremy.morel@opendatasoft.com>
2017-10-25 11:25:19 +0200

    change default protocol from http to https

--- config.example.js
+++ config.example.js
@@ -7,7 +7,7 @@ module.exports = {
     ODS_DEFAULT_LESS_WATCH_DIR: 'src/less/**',
 
     // You usually shouldn't have to edit these
-    ODS_PORTAL_PROTOCOL: 'http:',
+    ODS_PORTAL_PROTOCOL: 'https:',
     ODS_PORTAL_SUFFIX: '.opendatasoft.com',
     ODS_PORTAL_PORT: 80
 };
